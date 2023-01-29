3557f31e91d4587f87337f2b27082e5d4f084aee
佐藤昭文 <akfm.sato@gmail.com>
2023-01-29 15:13:50 +0900

    add error story

--- src/TodoApp.stories.tsx
+++ src/TodoApp.stories.tsx
@@ -1,5 +1,21 @@
+import { rest } from "msw";
 import TodoApp from "./TodoApp";
 
 export default { component: TodoApp };
 
 export const Default = {};
+
+export const ApiError = {
+  parameters: {
+    msw: [
+      rest.get("/api/todos", (_req, res, ctx) =>
+        res(
+          ctx.status(500),
+          ctx.json({
+            message: "test error",
+          })
+        )
+      ),
+    ],
+  },
+};
